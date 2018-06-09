bcc225ebf8b6c7d91e390fa20497d55b46c03a45
ntvirus <ntvirus333@gmail.com>
2018-06-09 20:10:35 +0545

    fitness calculation code updated, error handling left

--- mod_gen_algo.cpp
+++ mod_gen_algo.cpp
@@ -112,7 +112,7 @@ Schedule::Schedule(vector<int> chromosome){
 //mating function that produces offspring based on 2 parents
 Schedule Schedule::mate(Schedule parent2){
     //crossover and mutation goes here
-    vecto<int> child_chromosome;
+    vector<int> child_chromosome;
     //make the size of child same as the parent
     int len=chromosome.size();
     for (int i=0; i<len; ++i){
@@ -131,6 +131,45 @@ Schedule Schedule::mate(Schedule parent2){
 //main function that determines whether or not to accept the answer
 int Schedule::calcFitness(){
     //fitness calculation function goes here
+    map<int,int> mem;
+    int i=0;
+    for (int j=0; j<chromosome.size(); ++j)
+        mem[chromosome[j]]=0;
+
+    //initialize fitness value
+    int fit=0;
+
+    //code for no pair integer
+    while (i<chromosome.size()){
+        if (i==0){
+            ++mem[chromosome[i]];
+            if (chromosome[i+1]!=chromosome[i])
+                ++fit;
+            ++i;
+        }
+        else{
+            if (chromosome[i]==chromosome[i-1]){
+                ++i;
+            }
+            else{
+                if (i<chromosome.size()-1 and chromosome[i]!=chromosome[i+1])
+                    ++fit;
+                else if (i==chromosome.size()-1)
+                    ++fit;
+                mem[chromosome[i]]++;
+                ++i;
+            }
+        }
+    }
+
+    //code for repetitive integer
+    for (int i=0; i<chromosome.size(); ++i){
+        if(mem[chromosome[i]]!=0){
+            fit+=(mem[chromosome[i]]-1);
+            mem[chromosome[i]]=0;
+        }
+    }
+    return fit;
 }
 
 //display the schedule created
@@ -140,37 +179,39 @@ void Schedule::displaySchedule(){
 }
 
 int main(){
+    srand((unsigned)(time(0)));
 	string inp; //used to store console input
 	string nm;  //name of employee
 	int generation=0;   //keep count of generation
 	vector<Schedule> population;    //main population that evolves with time
 	int idVal,fst,lst;  //variables to hold employee details
-	bool found=false;   //to keep the loop running
+	bool found=false, quit=false;   //to keep the loop running
+	while(!quit){
+        /*The following commented code was used for console i/o*/
+        cout<<endl;
+        cout<<"Enter command:\n";
+        cin>>inp;
+        if(inp=="add"){
+            cout<<"About to add employee:\n";
+            cout<<"Enter name, id, start hour and end hour: \t";
+            cin>>nm>>idVal>>fst>>lst;
+            Employee *e=new Employee(nm,idVal,fst,lst);
+            domain.push_back(*e);
+        }
+        else if(inp=="show"){
+            for(int i=0; i<domain.size(); ++i){
+                domain[i].showDetails();
+            }
+        }
+        else if (inp=="quit")
+            quit=true;
+	}
 	for(int i=0; i<POPULATION_SIZE; ++i){
         vector<int> gnome=createGnome(WORKING_HOUR);    //creates a random schedule of length WORKING_HOUR
         population.push_back(Schedule(gnome));      //add the random schedule to population
 	}
+
 	while (!found){
-
-/*The following commented code was used for console i/o*/
-//        cout<<endl;
-//        cout<<"Enter command:\n";
-//        cin>>inp;
-//        if(inp=="add"){
-//            cout<<"About to add employee:\n";
-//            cout<<"Enter name, id, start hour and end hour: \t";
-//            cin>>nm>>idVal>>fst>>lst;
-//            Employee *e=new Employee(nm,idVal,fst,lst);
-//            domain.push_back(*e);
-//        }
-//        else if(inp=="show"){
-//            for(int i=0; i<domain.size(); ++i){
-//                domain[i].showDetails();
-//            }
-//        }
-//        else if (inp=="quit")
-//            quit=true;
-
         sort(population.begin(), population.end()); //sort according to fitness, lower fitness value -> best answer
         if (population[0].fitness<=0){      //if fitness=0, we've found the answer
         /* Here "<=" is used instead of "=="
@@ -198,9 +239,23 @@ int main(){
         }
         population=new_generation;      //new_generation becomes the current population
         cout<<"Generation: "<<generation<<"\t";
-        cout<<"Schedule: "<<population[0].display()<<"\t";
-        cout<<"Fitness: "<<population[0].fitness<<"\n";
+
+        cout<<"Schedule: ";
+        population[0].displaySchedule();
+        cout<<"\t";
+
+        cout<<"Fitness: "<<(population[0]).fitness<<"\n";
         ++generation;           //next generation
 	}
+	cout<<"SOLUTION:\n";
+
+    cout<<"Generation: "<<generation<<"\t";
+
+    cout<<"Schedule: ";
+    population[0].displaySchedule();
+    cout<<"\t";
+
+    cout<<"Fitness: "<<(population[0]).fitness<<"\n";
+
 	return 0;
 }
